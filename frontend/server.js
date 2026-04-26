b6ca0db0d05cad8bc16985168ffa20caf17824de
GabrielR1988 <121864164+GabrielR1988@users.noreply.github.com>
2026-04-26 17:17:54 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Bajar los escudos de seguridad para permitir el Iframe
+// Bajar los escudos de seguridad para permitir Iframes y recursos compartidos (CORS)
 app.use((req, res, next) => {
     res.removeHeader("X-Frame-Options");
     res.setHeader("Content-Security-Policy", "frame-ancestors *");
+    
+    // El pase libre para los archivos de diseño (js, css, etc.)
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type");
+    
     next();
 });
 
