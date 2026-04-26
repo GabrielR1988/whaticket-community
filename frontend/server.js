c40672bfbbcee2e4760018e329754ee1eff2e69b
GabrielR1988 <121864164+GabrielR1988@users.noreply.github.com>
2026-04-26 17:08:20 -0300

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Bajar los escudos de seguridad para permitir el Iframe
+app.use((req, res, next) => {
+    res.removeHeader("X-Frame-Options");
+    res.setHeader("Content-Security-Policy", "frame-ancestors *");
+    next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get(/(.*)/, function (req, res) {
