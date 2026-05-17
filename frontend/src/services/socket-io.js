c232a2370ffaa7195003484aafa960556af1180c
GabrielR1988 <121864164+GabrielR1988@users.noreply.github.com>
2026-05-17 01:26:26 -0300

    Update socket connection to use proxy URL
    Replaced getBackendUrl() with a direct proxy URL for socket connection.

--- frontend/src/services/socket-io.js
+++ frontend/src/services/socket-io.js
@@ -1,9 +1,11 @@
 import openSocket from "socket.io-client";
+// Podés dejar la línea de import de getBackendUrl, no molesta aunque ya no la usemos.
 import { getBackendUrl } from "../config";
 
 function connectToSocket() {
     const token = localStorage.getItem("token");
-    return openSocket(getBackendUrl(), {
+    // ACÁ ESTÁ LA MAGIA: Reemplazamos getBackendUrl() por tu proxy
+    return openSocket("https://mantenertoken-production.up.railway.app", {
       transports: ["websocket", "polling", "flashsocket"],
       query: {
         token: JSON.parse(token),
